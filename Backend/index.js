be3aca5bc4dbc18f666e8c7981963b5f7c19790e
Pankaj <pka936636@gmail.com>
2025-10-31 12:18:04 +0700

    /* add

--- Backend/index.js
+++ Backend/index.js
@@ -165,7 +165,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "./client/dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/dist/index.html"));
 });
 
